a4eb85615924d3284e3c17261bf2b2ea8c1f8810
Chris Gauthier <cpgauthier@gmail.com>
2012-01-12 14:30:25 -0500

    Added #message directive to narrative parser. Revised how parser handles prefixes in the metadata. Added conditional(?), anti-conditional(!), and message ($) prefixes. Changed state removal prefix to (-).

--- dialogue.js
+++ dialogue.js
@@ -12,9 +12,9 @@ PAVO.dialogue = new function() {
 
 	this.init = function(id) {
 		var entry = jQuery(id).html().split("\n");
-		var data, tag, id, root, sect;
+		var data, tag, id, ch, wd, root, sect;
 		var keyword, record;
-		var i, il, j, jl, phase = 0;
+		var i, il, j, jl, phase = 0, lookup = {};
 
 		for (i = 0, il = entry.length; i < il; i++) {
 			data = jQuery.trim(entry[i]);
@@ -40,6 +40,12 @@ PAVO.dialogue = new function() {
 						id = tag[1];
 						sect = root.section[id] = [];
 						break;
+						
+					case "#message":
+						id = tag[1];
+						tag.splice(0, 2);
+						lookup[id] = tag.join(" ");
+						break; 
 					}
 				
 				} else {
@@ -56,10 +62,36 @@ PAVO.dialogue = new function() {
 						tag = data.split(" ");
 						for (j = 0, jl = tag.length; j < jl; j++) {
 							id = tag[j];
-							if (id.charAt(0) === "@") {
-								record.transit = id.substring(1);
-							} else {
-								record.keyword.push(tag[j]);
+							ch = id.charAt(0);
+							wd = id.substring(1);
+							switch(ch) {
+							
+							case "?":
+								record.required = record.required || [];
+								record.required.push(wd);
+								record.keyword.push(wd);
+								break;
+								
+							case "!":
+								record.unwanted = record.unwanted || [];
+								record.unwanted.push(wd);
+								break;
+								
+							case "-":
+								record.deletion = record.deletion || [];
+								record.deletion.push(wd);
+								break;
+								
+							case "@":
+								record.transit = wd;
+								break;
+								
+							case "$":
+								record.message = lookup[wd];
+								break;
+								
+							default:
+								record.keyword.push(id);
 							}
 						}
 					} else {
@@ -71,60 +103,79 @@ PAVO.dialogue = new function() {
 		}
 	};
 
-	this.getCurrent = function(subject) {
+	this.getSection = function(subject) {
 		var npc = table[subject.name];
 		return npc.section[npc.current];
-	}
+	};
 	
-	this.setCurrent = function(subject, section) {
+	this.setSection = function(subject, section) {
 		var npc = table[subject.name];
 		npc.current = section;
-	}
-
-	this.getKeywords = function(subject) {
-		var i, il, j, jl, entry, word, list = [];
-		var section = this.getCurrent(subject);
+	};
+	
+	this.enumerate = function(subject, callback) {
+		var section = this.getSection(subject);
+		var i, il, entry;
 		for (i = 0, il = section.length; i < il; i++) {
 			entry = section[i];
-			if (!entry.visited) {
-				for (j = 0, jl = entry.keyword.length; j < jl; j++) {
-					word = entry.keyword[j];
-					if (state.contains(word)) {
-						list.add(word);
-					}
+			if (!entry.visited && 
+			(!entry.required || entry.required.match(state).length === entry.required.length) &&
+			(!entry.unwanted || entry.unwanted.match(state).length === 0)) {
+				if (!callback(entry)) {
+					break;
 				}
 			}
 		}
+	};
+
+	this.getKeywords = function(subject) {
+		var i, il, word, list = [];
+		this.enumerate(subject, function(entry) {
+			for (i = 0, il = entry.keyword.length; i < il; i++) {
+				word = entry.keyword[i];
+				if (state.contains(word)) {
+					list.add(word);
+				}
+			}
+			return true;
+		});
 		return list;
 	};
 
 	this.getResponse = function(subject, request) {
-		var section = this.getCurrent(subject);
-		var entry, i, il, st;
+		var section, entry, i, il, st;
 		
 		if (request) {
-		
-			for (i = 0, il = section.length; i < il; i++) {
-				entry = section[i];
-				if (!entry.visited && entry.keyword.contains(request)) {
-					break;
-				}
-			}
-		
+			this.enumerate(subject, function(e) {
+				entry = e;
+				return !e.keyword.contains(request);
+			});
 		} else {
+			section = this.getSection(subject);
 			entry = section[0];
 		}
 		
+		entry.visited = true;
+
 		for (i = 0, il = entry.keyword.length; i < il; i++) {
 			state.add(entry.keyword[i]);
 		}
-		entry.visited = true;
 
+		if (entry.deletion) {
+			for (i = 0, il = entry.deletion.length; i < il; i++) {
+				state.del(entry.keyword[i]);
+			}
+		}
+		
 		if (entry.transit) {
-			this.setCurrent(subject, entry.transit);
-			if (this.getCurrent(subject).length === 0) {
+			this.setSection(subject, entry.transit);
+			if (this.getSection(subject).length === 0) {
 				subject.active = false;
 			}
+		}
+
+		if (entry.message) {
+			PAVO.hud.addMessage(entry.message);
 		}
 
 		return entry.response;
